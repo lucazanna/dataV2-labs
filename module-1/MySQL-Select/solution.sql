e8ef62ccb76db3e54a73a1ef84a305402c5e5a47
lucazanna <lc@finance-bi.com>
2020-06-27 20:29:58 +0200

    done SQL exercise v2

--- module-1/MySQL-Select/solution.sql
+++ module-1/MySQL-Select/solution.sql
@@ -19,18 +19,15 @@ SELECT COUNT(*) FROM publications.titleauthor;
 # CHALLENGE 2
 SELECT authors.au_id AS AUTHOR_ID, authors.au_lname AS LAST_NAME,  authors.au_fname AS FIRST_NAME, publishers.pub_name AS PUBLISHER, COUNT(titles.title) AS TITLE_COUNT
 FROM authors 
-LEFT JOIN titleauthor
-ON titleauthor.au_id = authors.au_id
-LEFT JOIN titles
-ON titleauthor.title_id = titles.title_id
-LEFT JOIN publishers
-ON titles.pub_id = publishers.pub_id
+LEFT JOIN titleauthor ON titleauthor.au_id = authors.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
 GROUP BY authors.au_id, publishers.pub_id;
 
 #TOTAL OF 25 BOOKS FOR 23 AUTHORS
 
 # CHALLENGE 3
-SELECT authors.au_id AS AUTHOR_ID, authors.au_lname AS LAST_NAME,  authors.au_fname AS FIRST_NAME, COUNT(titles.title) AS TOTAL
+SELECT authors.au_id AS AUTHOR_ID, authors.au_lname AS LAST_NAME,  authors.au_fname AS FIRST_NAME, SUM(titles.ytd_sales) AS TOTAL
 FROM authors 
 LEFT JOIN titleauthor
 ON titleauthor.au_id = authors.au_id
@@ -42,7 +39,7 @@ LIMIT 0, 3
 
 
 # CHALLENGE 4
-SELECT authors.au_id AS AUTHOR_ID, authors.au_lname AS LAST_NAME,  authors.au_fname AS FIRST_NAME, COUNT(titles.title) AS TOTAL
+SELECT authors.au_id AS AUTHOR_ID, authors.au_lname AS LAST_NAME,  authors.au_fname AS FIRST_NAME, COALESCE(SUM(titles.ytd_sales),0) AS TOTAL
 FROM authors 
 LEFT JOIN titleauthor
 ON titleauthor.au_id = authors.au_id
